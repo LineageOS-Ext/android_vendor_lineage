0e7a1c41e7440207f55719eefa902c875b285073
NotBlazeee28 <NotBlazeee@gmail.com>
2024-05-21 13:04:47 +0530

    vendor: Drop few prebuilt stuffs

--- config/common_full.mk
+++ config/common_full.mk
@@ -9,11 +9,7 @@ $(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
 
 # Apps
 PRODUCT_PACKAGES += \
-    Eleven \
-    Etar \
-    Profiles \
-    Recorder \
-    Seedvault
+    Profiles 
 
 ifneq ($(PRODUCT_NO_CAMERA),true)
 PRODUCT_PACKAGES += \
